b370bc13d4c74d047afc561b556b3cfc4e66ec8e
Julia <veluat@gmail.com>
2023-09-18 21:16:33 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {Box} from "@mui/material";
+import {TreeView} from "@mui/x-tree-view";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import Tree from "./Tree";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    return (
+        <Box sx={{height: '100vh', width: '100vw'}}>
+            <TreeView
+                aria-label="rich object"
+                defaultCollapseIcon={<ExpandMoreIcon/>}
+                defaultExpanded={['root']}
+                defaultExpandIcon={<ChevronRightIcon/>}
+            >
+                <Tree/>
+            </TreeView>
+        </Box>
+    );
+};
 
 export default App;
